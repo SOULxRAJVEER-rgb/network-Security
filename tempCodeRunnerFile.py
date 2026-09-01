13c714cea6e9d0f90dabb6e896fb78c6832fc841
SOULxRAJVEER-rgb <rajveersinghvs2004@gmail.com>
2026-09-01 21:34:56 +0530

    Setup.py file ready

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+setup(
+#     name="NetworkSecurity",
+#     version="0.0.1",
+#     author="Krish Naik",
+#     author_email="krishnaik06@gmail.com",
+#     packages=find_packages(),
+#     install_requires=get_requirements()
+# )
